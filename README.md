42b04e8e46e6a7532c69c775abc56a008923cce7
arnabisaha346789 <arnabisaha346789@gmail.com>
2023-08-19 15:58:51 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,115 @@
+# Project 2: Movie Trailer Website
+### by Arnabi Saha
+
+Movie trailer website project.
+
+## Overview:
+
+A Python program that produces the HTML for a movie website which displays a number of movies and Tv series trailers. After website has been created, it will open automatically in your default web browser. Simply click on posters to view trailers.
+
+## Required Libraries and Dependencies:
+
+Python 3.x is required to run this project. The Python executable should be in your default path, in which the Python installer should have set. 
+
+Moreover pandas should be installed. 
+
+To install pandas open the terminal or command prompt and type:
+
+```bash
+pip install pandas
+```
+
+## Project contents:
+
+This project consists for the following files:
+
+* **entertainment_center.py** - main Python script to run.
+* **media.py** - contains the class Movie and class TvSeries that stores movie and Tv-series details.
+* **fresh_tomatoes.py** - creates the HTML file for the website (Udacity supplied and then tweaked)
+* **datafile_movie.csv** - contains the data of the movie trailers.
+* **datafile_tv_series.csv** - contains the data of the tv series trailers.
+* **website.html** - Output html file which was created when *entertainment_center.py* was run.
+* **Licence.txt** - Licencing information.
+* **_config.yml** - Configuration file for github theme.
+
+## Download:
+This project can be downloaded by [clicking here](https://github.com/pks9862728888/Movie_trailer_website_generator/archive/master.zip)
+
+## How to Run Project:
+
+Download the project zip file to your computer and unzip the file. Or clone this repository to your desktop by typing the following code in your terminal(*for Linux*) or command prompt(*for windows*):
+
+```bash
+git clone https://github.com/pks9862728888/Movie_trailer_website_generator.git
+```
+
+Navigate to the project directory and type in the following command:
+
+```bash
+python main.py
+```
+
+Your default browser should launch a new tab displaying the movie trailer website.
+
+## How to add new movie/Tv_series trailer into webpage:
+
+The data of new trailers can be directly added in respective `.csv` files and then objects are to be created by typing the following code in `entertainment_center.py` inside `main()`:
+
+If new movie trailers are to be added, then type under this following line:
+```bash
+# Creating movies objects
+```
+this following code:
+```bash
+
+movie_data = get_trailer_data(data_movie, "name_of_new_movie_added_in_csv_file")
+name_of_object = media.Movie(movie_data[0], movie_data[1], movie_data[2], movie_data[3], movie_data[4])
+```
+However if new Tv_series trailers are to be added, then type under this following line:
+```bash
+# Creating movie trailer website
+```
+this following code:
+
+```bash
+series_data = get_trailer_data(data_tv_series, "name_of_new_Tv_series_added_in_csv_file", 1)
+name_of_object = media.TvShow(series_data[0], series_data[1], series_data[2], series_data[3], series_data[4], series_data[5])
+```
+
+Then add the new created `name_of_object` in list:
+
+```bash
+# Creating movie trailer website
+fresh_tomatoes.open_movies_page([axl, tau, automata, extinction, stealth, maze_runner_the_death_cure,
+                                maze_runner_the_scorch_trials, the_maze_runner, interstellar, pacific_rim,
+                                pacific_rim_uprising, lucy, prometheus, edge_of_tomorrow, inception,
+                                westworld_s1, westworld_s2, intelligence_s1, intelligence_s2, minority_report,
+                                almost_human, name_of_object])
+```
+Then execute `entertainment_center.py`.
+
+## Output after running `entertainment_center.py`:
+
+[This](http://htmlpreview.github.com/?https://github.com/pks9862728888/Movie_trailer_website_generator/blob/master/webpage.html) is what it looks like when you run *entertainment_center.py*
+
+## Extra Credit Description:
+
+The following features were implemented to gain an extra credit from Udacity:
+
+* Added the storyline of the movie to the website.
+* Added the release date to the Movie class, which is also displayed on the website.
+* Changed the background color to create a dark blue theme for the website.
+* Changed text color to get attractive look.
+* Added Tv series trailers by inheriting Movie class(*i.e. implemented inheritance*).
+* Added trailer poster images border.
+* Fixed display problem caused when story_lines are of different lengths.
+
+## Vulnerabilities and Fixes:
+If new movie/Tv_series data is to be shown then sometimes the trailer posters might not align themselves properly in grid, especially if their story_lines are of different lengths(such as one `single-line story_line` and the other `multi-line story_line`).
+
+
+To ensure all the trailers align perfectly, the minimum length of characters of movie/Tv_series story_line should be at least 136 characters(*I have taken it as minimum number of characters*). However if the story_line is greater than 136 characters, then the exceeding characters are truncated.
+
+## References:
+1. [Markdown formatting for README.md](https://help.github.com/articles/basic-writing-and-formatting-syntax/)
+2. [Getting preview of Html Directly from Github](https://stackoverflow.com/questions/6551446/can-i-run-html-files-directly-from-github-instead-of-just-viewing-their-source)
---
+++ fresh_tomatoes.py
@@ -0,0 +1,213 @@
+import webbrowser
+import os
+import re
+
+
+# Styles and scripting for the page
+main_page_head = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Trailers!</title>
+
+    <!-- Bootstrap 3 -->
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    
+    <script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+    <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    
+    <style type="text/css" media="screen">
+        body {
+            padding-top: 80px;
+            background: #00081c;
+            margin-bottom: -7%;
+        }
+        #boxshadow {
+        position: relative;
+        box-shadow: 1px 2px 4px rgba(0, 0, 0, .5);
+        padding: 2px;
+        background: #f2ff00;
+        }
+        #boxshadow img {
+        width: 100%;
+        border: 1px solid #0f014c;
+        border-style: inset;
+        }
+        #boxshadow::after {
+        content: '';
+        position: absolute;
+        z-index: -1; /* hide shadow behind image */
+        box-shadow: 0 15px 20px rgba(9, 3, 3, 0.3);
+        width: 70%;
+        left: 15%; /* one half of the remaining 30% */
+        height: 100px;
+        bottom: 0;
+        }
+        #trailer .modal-dialog {
+            margin-top: 120px;
+            width: 700px;
+            height: 500px;
+            vertical-align: middle;
+        } 
+        .hanging-close {
+            position: absolute;
+            top: -12px;
+            right: -12px;
+            z-index: 9001;
+        }
+        #trailer-video {
+            width: 100%;
+            height: 100%;
+        }
+        .movie-tile {
+            margin-bottom: 5px;
+            padding-top: 10px;
+            color: #4ddbff;
+        }
+        .movie-tile:hover {
+            background-color: #43587a;
+            cursor: pointer;
+        }
+        .scale-media {
+            padding-bottom: 60%;
+            position: relative;
+        }
+        .scale-media iframe {
+            border: none;
+            height: 100%;
+            position: absolute;
+            width: 100%;
+            left: 0;
+            top: 0;
+            background-color: white;
+        }
+    </style>
+    <script type="text/javascript" charset="utf-8">
+        // Pause the video when the modal is closed
+        $(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+            // Remove the src so the player itself gets removed, as this is the only
+            // reliable way to ensure the video stops playing in IE
+            $("#trailer-video-container").empty();
+        });
+        // Start playing the video whenever the trailer modal is opened
+        $(document).on('click', '.movie-tile', function (event) {
+            var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
+            var sourceUrl = 'http://www.youtube.com/embed/' + trailerYouTubeId + '?autoplay=1&html5=1';
+            $("#trailer-video-container").empty().append($("<iframe></iframe>", {
+              'id': 'trailer-video',
+              'type': 'text-html',
+              'src': sourceUrl,
+              'frameborder': 0
+            }));
+        });
+        // Animate in the movies when the page loads
+        $(document).ready(function () {
+          $('.movie-tile').hide().first().show("fast", function showNext() {
+            $(this).next("div").show("fast", showNext);
+          });
+        });
+    </script>
+</head>
+'''
+
+
+# The main page layout and title bar
+main_page_content = '''
+  <body>
+    <!-- Trailer Video Modal -->
+    <div class="modal" id="trailer">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <a href="#" class="hanging-close" data-dismiss="modal" aria-hidden="true">
+            <img src="https://lh5.ggpht.com/v4-628SilF0HtHuHdu5EzxD7WRqOrrTIDi_MhEG6_qkNtUK5Wg7KPkofp_VJoF7RS2LhxwEFCO1ICHZlc-o_=s0#w=24&h=24"/>
+          </a>
+          <div class="scale-media" id="trailer-video-container">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Main Page Content -->
+    <div class="container">
+      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+          <div class="navbar-header">
+            <a class="navbar-brand" href="#">Movies and Tv Series Trailers</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      {movie_tiles}
+    </div>
+    <footer style="background-color:#141419; color:#5c5c70;">
+        <div class="container text-center">
+            <div class="rights">copyright &copy; 2023 Arnabi Saha</div>
+            <div class="rights">licensed under <a href="https://choosealicense.com/licenses/mit/" target="_blank">CC BY MIT</a></div>
+            <div class="rights">Contact Us:</div>
+            <div class="rights"><a href="https://www.linkedin.com/in/Arnabi Saha/">LinkedIn</a></div>
+            <div class="rights">arnabisaha90@gmail.com</div>
+            <div>&bull; &bull; &bull;</div>
+            <div class="disclaimer">All webpages appearing in this work are fictitious and for project purposes only. Any resemblance to real websites, living or dead, is purely coincidental.</div>
+        </div>  
+    </footer>
+  </body>
+</html>
+'''
+
+
+# A single movie entry html template
+movie_tile_content = '''
+<div class="col-md-6 col-lg-4 match-my-cols movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
+    <img src="{poster_image_url}" width="200" height="300" id="boxshadow">
+    <h4><b><font color="red">{movie_title}</font></b></h4>
+    <p style="font-size:13px"><b>Release Date: </b>{release_date}</p>
+    <p style="font-size:13px">{movie_story_line}</p>
+</div>
+'''
+
+
+def create_movie_tiles_content(movies):
+    """ Creates moves tiles in web-page. """
+    # The HTML content for this section of the page
+    content = ''
+    for movie in movies:
+        # Extract the youtube ID from the url
+        youtube_id_match = re.search(
+            r'(?<=v=)[^&#]+', movie.you_tube_trailer_url)
+        youtube_id_match = youtube_id_match or re.search(
+            r'(?<=be/)[^&#]+', movie.you_tube_trailer_url)
+        trailer_youtube_id = (youtube_id_match.group(0) if youtube_id_match
+                              else None)
+
+        # Append the tile for the movie with its content filled in
+        content += movie_tile_content.format(
+            movie_title=movie.title,
+            movie_story_line=movie.story_line,
+            poster_image_url=movie.poster_image_url,
+            trailer_youtube_id=trailer_youtube_id,
+            release_date=movie.release_date
+        )
+    return content
+
+
+def open_movies_page(movies):
+    """ Opens movies page and/appends new movies into this page."""
+    # Create or overwrite the output file
+    output_file = open('webpage.html', 'w')
+
+    # Replace the movie tiles placeholder generated content
+    rendered_content = main_page_content.format(
+        movie_tiles=create_movie_tiles_content(movies))
+
+    # Output the file
+    output_file.write(main_page_head + rendered_content)
+    output_file.close()
+
+    # open the output file in the browser (in a new tab, if possible)
+    url = os.path.abspath(output_file.name)
+    webbrowser.open('file://' + url, new=2)
